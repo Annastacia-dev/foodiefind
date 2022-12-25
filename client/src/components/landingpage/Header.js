e54c301d8a8a8b6c3669fffea39a7e1573580ae0
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-25 10:40:35 +0300

    change header styling

--- client/src/components/landingpage/Header.js
+++ client/src/components/landingpage/Header.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import '../../css/landingpage/Header.css'
+import NavBar from './NavBar'
 import foodpng from '../../images/landingpage/foodie.png'
 
 const Header = () => {
   return (
-    <div className="container fluid header">
-      <div className="row glass">
-        <div className="col-md-6">
-          <div className="header-text">
-            <h1>Explore culinary
-             <span> experiences</span>
-            </h1>
-            <p>
-            Find and book the perfect restaurant for any occasion on our app. Browse ratings and reviews, create lists of your favorite spots, and connect with other foodies. Discover the best restaurants in your city and make reservations at your favorite spots, all in one place.
-            </p>
+  // bootstrap header
+    <div className="header">
+      <NavBar />
+      <div className="container"> 
+        <div className="row">
+          <div className="col-md-6">
+            <div className="header-content">
+              <h1 className="header-title">
+                Explore new culinary experiences.
+              </h1>
+              <p className="header-text">
+              Find and book the perfect restaurant for any occasion on our app. Browse ratings and reviews, create lists of your favorite spots, and connect with other foodies. Discover the best restaurants in your city and make reservations at your favorite spots, all in one place.
+              </p>
+              <a href="#about" className="btn btn-outline-light btn-lg">Get Started</a>
+            </div>
           </div>
-          <div className="getstarted">
-            <button className="btn btn-primary">Get Started</button>
+          <div className="col-md-6">
+            <div className="header-image">
+              <img src={foodpng} alt="foodie" />
+            </div>
           </div>
         </div>
-        <div className="col-md-6">
-          <div className="foodimage">
-            <img src={foodpng} alt="foodie" />
-          </div>
-        </div>
-        </div>
+      </div>
     </div>
-    
   )
 }
 
