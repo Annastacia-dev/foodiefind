3d47cfd04f1db3f72cd3a0b0618d52bd980f25a7
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-24 17:52:45 +0300

    restaurants & reviews controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+
+    def index 
+        @restaurants = Restaurant.all
+        render json: @restaurants
+    end
+
+    def show
+        @restaurant = Restaurant.find(params[:id])
+        render json: @restaurant
+    end
+
+    def create
+        @restaurant = Restaurant.create!(restaurant_params)
+        render json: @restaurant
+    end
+
+    def update
+        @restaurant = Restaurant.find(params[:id])
+        @restaurant.update!(restaurant_params)
+        render json: @restaurant
+    end
+
+    def destroy
+        @restaurant = Restaurant.find(params[:id])
+        @restaurant.destroy
+        render json: { message: "Restaurant deleted" }, status: :accepted
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :location, :cuisines, :price_range, :cover_image, :description,:phone_number, :website, :email, :opening_hours, :closing_hours, :payment_options, :delivery_options, :parking_options, :delivery_options, :takeout_options, :outdoor_seating, :has_wifi, :photos, :menu)
+    end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,40 @@
 class ReviewsController < ApplicationController
+
+    def index 
+        @reviews = Review.all
+        render json: @reviews
+    end
+
+    def show
+        @review = Review.find(params[:id])
+        render json: @review
+    end
+
+    def create
+        @review = Review.create!(review_params)
+        render json: @review
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update!(review_params)
+        render json: @review
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        render json: { message: "Review deleted" }, status: :accepted
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :content, :meal, :date_of_visit, :service_rating, :food_rating, :ambience_rating, :overall_rating, :photos, :user_id, :restaurant_id)
+    end
+
+
+
+
+
 end
