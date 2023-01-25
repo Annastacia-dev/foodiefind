e22d5be63ea32454bbe9c0fffaa8c1db99277f93
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-25 15:24:46 +0300

    assign dollar sign depending on price range

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,14 @@ class Restaurant < ApplicationRecord
     has_many :users, through: :reviews
 
 
+    def price_range_dollars
+        price_range = self.price_range
+        price_range = price_range.split("-")
+        price_range = price_range[1].to_i
+        price_range = price_range / 3000
+        price_range = "$" * price_range
+        price_range
+    end
+   
+
 end
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,8 +1,9 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :cuisines, :price_range, :cover_image, :description,:phone_number, :website, :email, :opening_hours, :closing_hours, :payment_options, :delivery_options, :parking_options, :delivery_options, :takeout_options, :outdoor_seating, :has_wifi, :photos, :menu, :open_days, :facebook, :instagram, :twitter
+  attributes :id, :name, :location, :cuisines, :price_range, :cover_image, :description,:phone_number, :website, :email, :opening_hours, :closing_hours, :payment_options, :delivery_options, :parking_options, :delivery_options, :takeout_options, :outdoor_seating, :has_wifi, :photos, :menu, :open_days, :facebook, :instagram, :twitter, :price_range_dollars
 
   has_many :reviews
   has_many :users, through: :reviews
+
 
 
 
